b8abaa6c18e1d23252bbf08d15ae84aa2de0c067
SashaCeretti <cerettisasha@gmail.com>
2021-07-14 16:18:37 -0300

    ejercicio 9 de if resuelto

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,18 @@
-function mostrar()
-{
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+function mostrar() {
+  let max = 10;
+  let min = 1;
 
-}//FIN DE LA FUNCIÓN
+  let numero = Math.floor(Math.random() * (max - min + 1) + min);
+
+  console.log(numero);
+
+  //si yo ya sé que max es 10 y min es 1, reemplazo directamente por los valores
+
+  //let numero = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+
+  //simplifico más
+
+  //let numero = Math.floor(Math.random() * 10 + 1);
+
+  //console.log(numero);
+}
