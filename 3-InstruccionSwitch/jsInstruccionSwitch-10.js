b8affc1241394d3076f3436906175091f468ea5b
SashaCeretti <cerettisasha@gmail.com>
2021-07-17 18:47:21 -0300

    ejercicio 10 de switch resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,42 @@
-function mostrar()
-{
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+function mostrar() {
+  let estacion;
+  let destino;
 
-}//FIN DE LA FUNCIÓN
+  estacion = document.getElementById("txtIdEstacion").value;
+  destino = document.getElementById("txtIdDestino").value;
+
+  switch (estacion) {
+    case "Invierno":
+      if (destino !== "Bariloche") {
+        alert("No se viaja");
+      } else {
+        alert("Se viaja");
+      }
+      break;
+
+    case "Verano":
+      if (destino == "Mar del plata" || destino == "Cataratas") {
+        alert("Se viaja");
+      } else {
+        alert("No se viaja");
+      }
+      break;
+
+    case "Otoño":
+      alert("Se viaja");
+
+      break;
+
+    case "Primavera":
+      if (destino !== "Bariloche") {
+        alert("Se viaja");
+      } else {
+        alert("No se viaja");
+      }
+      break;
+  }
+}
+
+// hacer una unica variable ''mensaje'' (muy complejo para mi, quedarme con mi codigo)
+// si no hay estructura condicional (), no hace falta poner llaves para un alert
+// ATENCION LA VARIABLE FLAG - ver video clase hacia ultimos momentos
