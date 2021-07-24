5192af51f6d6e098abded47cd2ec0f6a294a997a
SashaCeretti <cerettisasha@gmail.com>
2021-07-24 19:31:58 -0300

    ejercicios 7 y 8 de while resueltos

--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -2,17 +2,39 @@
 Al presionar el botón pedir  números  hasta que el USUARIO QUIERA 
 e informar la suma acumulada y el promedio.
 */
-function mostrar()
-{
-	var contador;
-	var acumulador;
-	var respuesta;
-	contador=0;
-	acumulador=0;
-	respuesta='si';
+function mostrar() {
+  let numero;
+  let acumulador = 0;
+  let contador = 0;
+  let promedio;
+  let respuesta;
 
+  do {
+    numero = parseInt(prompt("Ingrese un número:"));
+    while (isNaN(numero)) {
+      numero = parseInt(prompt("Eso no es un número, ingrese un número:"));
+    }
 
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/contador;
+    acumulador = acumulador + numero;
 
-}//FIN DE LA FUNCIÓN
+    contador++;
+
+    respuesta = prompt("Desea ingresar otro número? s / n");
+  } while (respuesta == "s");
+
+  promedio = acumulador / contador;
+
+  document.getElementById("txtIdSuma").value = acumulador;
+
+  document.getElementById("txtIdPromedio").value = promedio;
+}
+
+/*
+do(){   //hacé.... mientras.... el do while me permite entrar por lo menos una vez y al final estará la condicion
+
+} while(condicion); SIEMPRE PUNTO Y COMA AL FINAL DEL WHILE DEL DOWHILE
+
+----
+
+for
+*/
--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -1,19 +1,56 @@
-/*
+/* EJERCICIO 8 DE WHILE
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 sumar los que son positivos y multiplicar los negativos.*/
+
+//EL TRABAJO MAS IMPORTANTE PARA UN PROGRAMADOR: ENTENDER EL ENUNCIADO
+//Y ESCRIBIR LA ESTRATEGIA EN LENGUAJE HUMANO,
+//Y RECIEN DESPUES ESCRIBIR EL CODIGO EN LENGUAJE MAQUINA
+
 function mostrar()
 {
-	var contador;
-	var respuesta;
-	var sumaPositivos;
-	var multiplicacionNegativos;
-	contador=0;
-	sumaPositivos=0;
-	multiplicacionNegativos=1;
-	respuesta='si';
+	//declaro variables
+	let numero;
+	let respuesta;
+	let acumuladorPositivo = 0; //porque sumamos, es para empezar, cualquier número más 0 da ese número
+	let acumuladorNegativo = 1; //porque multiplicamos, es para empezar, cualquier número por 1 da ese número
+	let flag = 0;
+	
+	//tengo que generar un bucle del tipo mientras el usuario quiera(do-while)
+	//cosas que se tienen que hacer dentro del bucle (o sea mientras el usuario quiera)
+	//dentro del bucle: pido un numero, valido que sea numero, analizo el signo del numero
 
+	do{
+		numero = parseInt(prompt("Ingrese un número positivo o negativo"));
+		while(isNaN(numero)){
+			numero = parseInt(prompt("Eso no es número, ingrese número positivo o negativo"));
+		}
 
-	txtIdSuma.value=sumaPositivos;
-	txtIdProducto.value=multiplicacionNegativos;
+		if (numero >= 0){
+			
+			//si el numero es positivo lo sumo a los demas de positivos
+			acumuladorPositivo = acumuladorPositivo + numero;
 
-}//FIN DE LA FUNCIÓN
+		}
+		else{
+			flag = 1; //uso un flag con distinto valor para saber si flujo de programa entró acá
+			//si es negativo lo multiplico con los demas negativos
+			acumuladorNegativo = acumuladorNegativo * numero;
+		}
+
+		respuesta = prompt("Desea ingresar otro número? s / n")
+
+	}while(respuesta == "s");
+
+	if(!flag){
+		acumuladorNegativo = 0;
+	}
+
+	//afuera del bucle: despues de que el usuario no quiere ingresar mas valores
+	//muestro los resultados
+	document.getElementById("txtIdSuma").value = acumuladorPositivo;
+	document.getElementById("txtIdProducto").value = acumuladorNegatitivo;
+
+	//flag = 0
+	//entonces !flag sería no cero
+
+}
